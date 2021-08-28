d29ec98a68a62b7bbc542ca80758009431de5c15
Ilya <sheverdinkin@gmail.com>
2021-08-28 12:55:40 +0300

    add new tooling

--- src/app.js
+++ src/app.js
@@ -6,6 +6,17 @@ const app = express();
 const bodyParser = require('body-parser');
 const jsonParser = bodyParser.json();
 
+const winston = require("winston");
+
+const logger = winston.createLogger({
+  level: "info",
+  format: winston.format.json(),
+  transports: [
+    new winston.transports.File({ filename: "logs/error.log", level: "error" }),
+    new winston.transports.File({ filename: "logs/combined.log" }),
+  ],
+});
+
 module.exports = (db) => {
     app.get('/health', (req, res) => res.send('Healthy'));
 
